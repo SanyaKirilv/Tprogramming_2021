cfa4dfe2975022b5550cd1133f8d54b28085fed9
Alexander <71167144+SanyaKirilv@users.noreply.github.com>
2022-02-06 18:49:58 +0300

    delete something

--- CourseApp/MainFile.cs
+++ CourseApp/MainFile.cs
@@ -7,15 +7,6 @@ namespace CourseApp
     {
         public static void Main(string[] args)
         {
-            var taskUserInterface = new TaskUserInterface();
-            taskUserInterface.StartPage();
-            var classUserInterface = new ClassUserInterfaceClass();
-            classUserInterface.StartPage();
-            var dataUserInterface = new DataUserInterface();
-            dataUserInterface.StartPage();
-            var zooUserInterface = new ZooUserInterface();
-            var zoo = new Zoo("Moscow zoo");
-            zooUserInterface.Draw(zoo);
             var game = new Game(16);
             game.Draw();
             Console.ReadLine();
